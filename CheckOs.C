17369af794136fb1b8d70b6af4ace4e1c60f30d5
ƒkS124 <57041516+hilty-ux@users.noreply.github.com>
2021-02-18 17:29:12 +0100

    Update CheckOs.C

--- CheckOs.C
+++ CheckOs.C
@@ -2,15 +2,12 @@
 
 int checkOS() {
     #ifdef _WIN32;
-        printf("true");
         return true;
     #endif;
-        printf("false");
         return false;
 
 }
 
 int main() {
-    checkOS();
     return 0;
 }
