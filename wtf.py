71f0af787cc98b7df260877bce8c5a77a51c1eb9
ƒkS124 <57041516+hilty-ux@users.noreply.github.com>
2021-02-25 13:21:52 +0100

    Delete wtf.py

--- wtf.py
+++
@@ -1,9 +0,0 @@
-import pyautogui
-
-pyautogui.hotkey('win', 'r')
-pyautogui.write('cmd')
-pyautogui.press('enter')
-pyautogui.moveTo(500, 500)
-pyautogui.click()
-pyautogui.write('shutdown /r')
-pyautogui.press('enter')
