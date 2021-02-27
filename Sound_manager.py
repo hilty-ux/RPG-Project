41b27803c13b3942b106fa05b23fa86902de95a8
ƒkS124 <57041516+hilty-ux@users.noreply.github.com>
2021-02-27 23:55:26 +0100

    Add files via upload

--- Sound_manager.py
+++ Sound_manager.py
@@ -5,20 +5,32 @@ import pygame.mixer
 class SoundManager:
 
     def __init__(self):
-        self.shield_hit_sound = pygame.mixer.Sound("assets/Sounds/shield_hit.mp3")
-        self.shield_hit_sound.set_volume(0.25)
+        try:
+            self.shield_hit_sound = pygame.mixer.Sound("assets/Sounds/shield_hit.mp3")
+            self.shield_hit_sound.set_volume(0.25)
 
-        self.swoosh_sound = pygame.mixer.Sound("assets/Sounds/swoosh.mp3")
-        self.swoosh_sound.set_volume(0.25)
+            self.swoosh_sound = pygame.mixer.Sound("assets/Sounds/swoosh.mp3")
+            self.swoosh_sound.set_volume(0.25)
 
-        self.sword_slice_sound = pygame.mixer.Sound("assets/Sounds/sword_slice.mp3")
-        self.sword_slice_sound.set_volume(0.25)
+            self.sword_slice_sound = pygame.mixer.Sound("assets/Sounds/sword_slice.mp3")
+            self.sword_slice_sound.set_volume(0.25)
+        except pygame.error:
+            pass
 
     def shield_hit(self):
-        self.shield_hit_sound.play()
+        try:
+            self.shield_hit_sound.play()
+        except Exception as e:
+            print(e)
 
     def swoosh(self):
-        self.swoosh_sound.play()
+        try:
+            self.swoosh_sound.play()
+        except Exception as e:
+            print(e)
 
     def slice(self):
-        self.sword_slice_sound.play()
+        try:
+            self.sword_slice_sound.play()
+        except Exception as e:
+            print(e)
